edf21abcf3243f2b61cdc53f2b9dc52dd2775942
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-11-17 22:42:52 +0530

    Added codes for arrow animation

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
 
     $('.navbar-nav li a').addClass('border-animation');
 
+    //Homepage Animation Button
+
+    $('.btn-sub1').hover(
+        function(){
+            $('.fa-angle-right').css("transform","rotate(180deg)");
+        },
+    );
 
 
 });
